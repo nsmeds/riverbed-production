2d92ef424b3840bd2288965fbbcb043e1ddcbd3d
nsmeds <niilosmeds@gmail.com>
2016-06-07 18:35:11 -0700

    initial commit

---
+++ app/transitions.js
@@ -0,0 +1,32 @@
+export default function(){
+    this.transition(
+      this.fromRoute('main'),
+      this.use('fade'),
+      this.reverse('fade')
+  );
+    this.transition(
+      this.fromRoute('menu.submit'),
+      this.use('crossFade'),
+      this.reverse('crossFade')
+  );  
+    this.transition(
+      this.fromRoute('menu.subscribe'),
+      this.use('crossFade'),
+      this.reverse('crossFade')
+  );  
+    this.transition(
+      this.fromRoute('menu.about'),
+      this.use('crossFade'),
+      this.reverse('crossFade')
+  );  
+    this.transition(
+      this.fromRoute('menu.contact'),
+      this.use('crossFade'),
+      this.reverse('crossFade')
+  );  
+    this.transition(
+      this.fromRoute('menu.back-issues'),
+      this.use('crossFade'),
+      this.reverse('crossFade')
+  );  
+}
